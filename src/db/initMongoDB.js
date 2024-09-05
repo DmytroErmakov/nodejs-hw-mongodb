9c4f93bbba02bc5ff8ca107e4bec307876125481
DmytroErmakov <demchik22@gmail.com>
2024-09-05 20:53:01 +0300

    without error

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 import { env } from '../utils/env.js';
 
-
 export const initMongoBD = async () => {
   try {
     const user = env('MONGODB_USER');
@@ -10,11 +9,8 @@ export const initMongoBD = async () => {
     const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
 
-    //   console.log({ user, password, url, db });
-
-    //   const DB_HOST = `mongodb+srv://demchik22:pZvDfDZh73OGXdBb@cluster0.rco66.mongodb.net/students?retryWrites=true&w=majority&appName=Cluster0`;
-      const DB_HOST = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
-   await mongoose.connect(DB_HOST);
+    const DB_HOST = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
+    await mongoose.connect(DB_HOST);
     console.log('MongoDB connection succesfully');
   } catch (error) {
     console.log('MongoDB connection error', error.message);
--- src/server.js
+++ src/server.js
@@ -1,43 +1,42 @@
-import express from "express";
-import cors from "cors";
-import pino from "pino-http";
-import { env } from "./utils/env.js";
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './utils/env.js';
 
 // це ми замінили ф-єю env
 // import dotenv from "dotenv";
 // dotenv.config();
 // const port = Number(process.env.PORT) || 3000;
 
-
 export const startServer = () => {
-    const app = express();
+  const app = express();
 
-    const logger = pino({
-        transport: {
-            target: "pino-pretty"
-        }
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  });
+
+  app.use(logger);
+  app.use(cors());
+  app.use(express.json());
+
+  // routes
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: `${req.url} not found`,
     });
+  });
 
-    app.use(logger);
-    app.use(cors());
-    app.use(express.json());
-
-    // routes
-
-    app.use((req, res) => {
-        res.status(404).json({
-            message: `${req.url} not found`
-        });
+  app.use((error, req, res, next) => {
+    res.status(500).json({
+      message: error.message,
     });
+  });
 
-    app.use((error, req, res, next) => {
-        res.status(500).json({
-            message: error.message,
-        });
-    });
+  // виклткаємо ф-цію env
+  const port = Number(env('PORT', 3000));
 
-    // виклткаємо ф-цію env
-    const port = Number(env("PORT", 3000));
-
-    app.listen(port, console.log("Server running on port 3000"));
+  app.listen(port, console.log('Server running on port 3000'));
 };
