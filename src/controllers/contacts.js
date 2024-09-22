62ed74cb5621771fe40efb6ac6b6cb8319e1de49
DmytroErmakov <demchik22@gmail.com>
2024-09-22 15:25:42 +0300

    upd hw3-crud

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -3,51 +3,77 @@ import createHttpError from 'http-errors';
 import * as contactServices from '../services/contacts.js';
 
 export const getAllContactsController = async (req, res) => {
-//   try {
-    const data = await contactServices.getAllContacts();
+  const data = await contactServices.getAllContacts();
 
-    res.json({
-      status: 200,
-      message: 'Successsfully found contacts',
-      data,
-    });
-//   } catch (error) {
-//       next(error);
-    // res.status(500).json({
-    //   message: error.message,
-    // });
-//   }
+  res.json({
+    status: 200,
+    message: 'Successsfully found contacts',
+    data,
+  });
 };
 
 export const getContactsByIdController = async (req, res) => {
-//   try {
-    const { id } = req.params;
-    const data = await contactServices.getContactById(id);
+  const { id } = req.params;
+  const data = await contactServices.getContactById(id);
 
-      if (!data) {
-        throw createHttpError(404, `Contact with id=${id} not found`);
-               //замість нижчевказаних 3 рядків
-               //   const error = new Error(`Contact with id=${id} not found`);
-               //   error.status = 404;
-               //   throw error;
-        //замість нижчевказаних 3 рядків
-        //   return res.status(404).json({
-        //     message: `Contact with id=${id} not found`,
-        //   });
-      }
+  if (!data) {
+    throw createHttpError(404, `Contact with id=${id} not found`);
+  }
 
-    res.json({
-      status: 200,
-      message: `Contactt wit ${id} successfully find`,
-      data,
-    });
+  res.json({
+    status: 200,
+    message: `Contactt with ${id} successfully find`,
+    data,
+  });
+};
 
-//   } catch (error) {
-//       next(error);
+export const addContactController = async (req, res) => {
+  const data = await contactServices.createContact(req.body);
 
-    //   const { status = 500, message } = error;
-    // res.status(status).json({
-    //   message,
-    // });
-//   }
+  res.status(201).json({
+    status: 201,
+    message: 'Contact add successfully',
+    data,
+  });
+};
+
+export const upsertContactController = async (req, res) => {
+  const { id } = req.params;
+  const { isNew, data } = await contactServices.updateContact(
+    { _id: id },
+    req.body,
+    { upsert: true },
+  );
+
+  const status = isNew ? 201 : 200;
+
+  res.json(status)({
+    status,
+    message: 'Contact upsert successfully',
+    data,
+  });
+};
+
+export const patchContactController = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactServices.updateContact({ _id: id }, req.body);
+
+  if (!result) {
+    throw createHttpError(404, `Contact with id=${id} not found`);
+  }
+  res.json({
+    stattus: 200,
+    message: 'Contact patched successfully',
+    data: result.data,
+  });
+};
+
+export const deleteContactController = async (req, res) => {
+  const { id } = req.params;
+  const data = await contactServices.deleteContact({ _id: id });
+
+  if (!data) {
+    throw createHttpError(404, `Contact with id=${id} not found`);
+  }
+  req.status(204).send();
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,13 +1,23 @@
 import {Router} from "express";
 
-import { getAllContactsController, getContactsByIdController } from "../controllers/contacts.js";
+// import { addContactController, getAllContactsController, getContactsByIdController, upsertContactController } from "../controllers/contacts.js";
+import * as contactControllers from "../controllers/contacts.js";
+
 
 import ctrlWrapper from "../utils/ctrlWrapper.js";
 
 const contactsRouter = Router();
 
-contactsRouter.get('/', ctrlWrapper(getAllContactsController));
+contactsRouter.get('/', ctrlWrapper(contactControllers.getAllContactsController));
 
-contactsRouter.get('/:id', ctrlWrapper(getContactsByIdController));
+contactsRouter.get('/:id', ctrlWrapper(contactControllers.getContactsByIdController));
+
+contactsRouter.post("/", ctrlWrapper(contactControllers.addContactController));
+
+contactsRouter.put("/:id", ctrlWrapper(contactControllers.upsertContactController));
+
+contactsRouter.patch("/:id", ctrlWrapper(contactControllers.patchContactController));
+
+contactsRouter.delete("/:id", ctrlWrapper(contactControllers.deleteContactController));
 
 export default contactsRouter;
